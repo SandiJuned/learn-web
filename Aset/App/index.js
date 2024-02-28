08c2e2e10874152ff82b891ed9a014b48f16a487
SandiJuned <junaedisandi784@gmail.com>
2024-02-28 09:01:09 +0700

    menambah npm

--- Aset/App/index.js
+++ Aset/App/index.js
@@ -1,10 +1,9 @@
-/*variable*/
+import * as THREE from '../../node-modules/three/build/three.module.js'
+
 let scane = new THREE.Scane(); //variable scane
 let cam = new THREE.PerspectiveCamera(45,innerWidth/innerHeight, 1 , 100 ); //variabel kamera
-let renderer = new THREE.WEBGLRenderer(); //variabel render
+let renderer = new THREE.WEBGLRenderer(); //variabel renderer
 
 renderer.setSize(innerWidth, innerHeight );
 document.body.appenChild(renderer.domElement);
 renderer.render(scane , cam);
-
-
